023290164429fb51f1f4a7ec02dc295d66f23799
Flotuz <flotuz10@gmail.com>
2020-12-23 00:37:27 -0600

    Se pulió el get de login para una mejor comprensión del lado del front

--- userService.js
+++ userService.js
@@ -1,7 +1,8 @@
 //Librerias
+//const faker = require("faker");
 const express = require("express");
 const app = express();
-//const faker = require("faker");
+const cors = require("cors");
 
 //Acceso a la BD
 const db = require("./data/conector");
@@ -10,39 +11,76 @@ const db = require("./data/conector");
 const hostname = "127.0.0.1";
 const port = "4000";
 
-app.get("/users", (request, response) => {
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    ); // If needed
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "X-Requested-With,content-type"
+    ); // If needed
+    res.setHeader("Access-Control-Allow-Credentials", false); // If needed
+    next();
+});
+
+//Se retorna el usuario si la contraseña es correcta
+app.get("/login", (request, response) => {
     let acceso = false;
 
-    let { correo, password } = request.query
-    
-    db.query(`select * from escuela.basico.users where correo = '${correo}'`, (error, results) => {
-        if (error) {
-            throw error;
+    //vaciamos lo que hay en el query
+    let { correo, password } = request.query;
+
+    db.query(
+        `select * from escuela.basico.users where correo = '${correo}'`,
+        (error, results) => {
+            //Si hay error en la BD
+            if (error) {
+                throw error;
+            }
+            //Si me mandan datos vacios
+            if (results.rows.length === 0) {
+                response.json({
+                    message: "Ingrese los datos a validar",
+                });
+            } else {
+                //Si la contraseña de la bd es igual a la contraseña de la query
+                if (results.rows[0].password === password) {
+                    acceso = true;
+                    //Manda el correo como un json
+                    console.log("ingresó un usuario");
+                    response.json({
+                        access: acceso,
+                        correo: results.rows[0].correo
+                    });
+                } else {
+                    console.log("Un usuario intentó ingresar");
+
+                    //Si algo está mal manda un mensaje
+                    response.json({
+                        message: "Revisa tus credenciales",
+                        access: acceso
+                    });
+                }
+            }
         }
-        //Si la contraseña de la bd es igual a la contraseña de la query
-        if(results.rows[0].password === password){
-            acceso = true;
-            //Manda el correo como un json
-            response.json({correo: results.rows[0].correo});
-        }else{
-            //Si algo está mal manda un mensaje
-            response.json({message: "Revisa tus credenciales"})
-        }
-    });
+    );
 });
 
 app.post("/nuevoUsuario", (request, response) => {
-    const { correo, password } = request.query
+    const { correo, password } = request.query;
 
     //let correo = faker.internet.email();
     //let password = faker.random.word();
 
+    //Para dar de alta un usuario
     db.query(
         `INSERT INTO escuela.basico.users (correo, password) VALUES ($1, $2)`,
         [correo, password],
         (error) => {
             if (error) {
-                throw error;
+                console.error(error);
             }
             response.status(201).end();
 
@@ -50,7 +88,6 @@ app.post("/nuevoUsuario", (request, response) => {
             //console.log(` {correo: ${correo}, password:${password}} dado de alta`);
         }
     );
-
 });
 
 app.get("/", (request, response) => {
